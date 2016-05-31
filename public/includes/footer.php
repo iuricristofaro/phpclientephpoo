64bfef9d9199b8253f44fa59f1602d891905229c
Iuri Cristofaro <iuri@iuriCristofaro.com.br>
2016-05-31 14:59:09 -0300

    assets

---
+++ public/includes/footer.php
@@ -0,0 +1,8 @@
+</body>
+</html>
+
+<!-- jQuery -->
+<script src="public/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="public/js/bootstrap.min.js"></script>
---
+++ public/includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="content-language" content="pt-BR" />
+        <title>Cadastro de Clientes</title>
+        <link href="public/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+<body>
